1dab3d84664a3f084d3d3d4480919c889fff86a7
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2018-08-19 00:49:44 +0300

    Unit Tests

---
+++ Metrics_Track/Metrics_Track.Test/Controllers/Admin/CountriesControllerTest.cs
@@ -0,0 +1,73 @@
+﻿namespace Metrics_Track.Test.Controllers.Admin
+{
+    using Metrics_Track.Services.Contracts;
+    using Metrics_Track.Web;
+    using Metrics_Track.Web.Areas.Admin.Controllers;
+    using Metrics_Track.Web.Areas.Admin.Models.Countries;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using System;
+    using System.Linq;
+
+    [TestClass]
+    public class CountriesControllerTest
+    {
+        [TestMethod]
+        public void CountriesController_ShouldBeInAdminArea()
+        {
+            var controller = typeof(CountriesController);
+
+            var areaAttribute = controller
+                    .GetCustomAttributes(true)
+                    .FirstOrDefault(a => a.GetType() == typeof(AreaAttribute))
+                    as AreaAttribute;
+
+            Assert.IsNotNull(areaAttribute);
+
+            Assert.AreEqual(areaAttribute.RouteValue, WebConstants.AdminArea);
+        }
+
+        [TestMethod]
+        public void CountriesController_ShouldBeOnlyForAdminUsers()
+        {
+            var controller = typeof(CountriesController);
+
+            var areaAttribute = controller
+                    .GetCustomAttributes(true)
+                    .FirstOrDefault(a => a.GetType() == typeof(AuthorizeAttribute)) as AuthorizeAttribute;
+
+            Assert.IsNotNull(areaAttribute);
+
+            Assert.AreEqual(areaAttribute.Roles, WebConstants.AdministratorRole);
+        }
+
+        [TestMethod]
+        public void GetAddCountry_ShouldReturnViewWithValidModel()
+        {            
+            var service = new Mock<ICountry>();
+
+            var controller = new CountriesController(service.Object);
+
+            var result = controller.AddCountry();
+
+            Assert.IsInstanceOfType(result, typeof(IActionResult));
+            
+            ViewResult model = result as ViewResult;
+
+            Assert.IsInstanceOfType(model.Model, typeof(AddCountryViewModel));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void PostAddCountry_ShouldThrowException()
+        {
+            var service = new Mock<ICountry>();
+
+            var controller = new CountriesController(service.Object);
+
+            var result = controller.AddNewCountryToList(new AddCountryViewModel());
+        }
+    }
+}
---
+++ Metrics_Track/Metrics_Track.Test/Controllers/DashboardContollerTest.cs
@@ -0,0 +1,73 @@
+﻿namespace Metrics_Track.Test.Controllers
+{
+    using Metrics_Track.Data.Models;
+    using Metrics_Track.Services.Contracts;
+    using Metrics_Track.Web.Controllers;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class DashboardContollerTest
+    {
+        private const string FirstUserId = "1";
+        private const string FirstUserUsername = "First";
+        private const string SecondUserId = "2";
+        private const string SecondUserUsername = "Second";
+
+        [TestMethod]
+        public async Task DashboardContoller_ShoudReturnRedirectToActionWhenIsAuthenticated()
+        {
+            var usr = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                 new Claim(ClaimTypes.NameIdentifier, "1"),
+                 new Claim(ClaimTypes.Name, string.Empty)
+            }));
+
+            var userManager = this.GetUserManagerMock();
+            var controller = new DashboardController(new Mock<ICountry>().Object,
+                                                     new Mock<IMining>().Object,
+                                                     new Mock<ITransaction>().Object,
+                                                     new Mock<IPendingList>().Object,
+                                                     userManager.Object);
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = usr }
+            };
+
+            ViewResult result = await controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+
+            Assert.IsInstanceOfType(result, typeof(IActionResult));
+        }
+
+        private Mock<UserManager<User>> GetUserManagerMock()
+        {
+            var userManager = UserManagerMock.New;
+            userManager
+                .Setup(u => u.GetUsersInRoleAsync(It.IsAny<string>()))
+                .ReturnsAsync(new List<User>
+                {
+                    new User { Id = FirstUserId, UserName = FirstUserUsername },
+                    new User { Id = SecondUserId, UserName = SecondUserUsername }
+                });
+
+            return userManager;
+        }
+    }
+
+    public class UserManagerMock
+    {
+        public static Mock<UserManager<User>> New
+            => new Mock<UserManager<User>>(
+                Mock.Of<IUserStore<User>>(), null, null, null, null, null, null, null, null);
+    }
+
+}
