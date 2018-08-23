fb420126a67ab959b7e2ad0784efe7a3d03e7913
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2018-08-23 21:50:08 +0300

    20180820174847_SeedDataToDatabase scripts

---
+++ Metrics_Track/Metrics_Track.Scripts/ScalarValuedFunctions.Script.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION [CPS].[SSC_GetSLAHrs](@OrigId int,@RD DateTime, @CD DateTime, @TL Varchar(50),@SLAType Varchar(50), @Priority Varchar(50), @Country Varchar(255)) Returns float
+Begin
+	--Implementation
+	Return 0
+End
+
+GO
+
+CREATE Function [CPS].[SSC_GetWorkingMin](@OrigId int,@Start DateTime, @End DateTime) Returns float
+Begin
+	--Implementation
+    Return 0
+End
